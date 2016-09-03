bb8729ae53ec5751e6f4ee20fcebecafb09516a7
eliz40x <me@eliza.link>
2016-09-03 21:01:36 +0900

    Update Hamlet.hs: Remove unused functions

--- src/Hakyll/Web/Hamlet.hs
+++ src/Hakyll/Web/Hamlet.hs
@@ -8,18 +8,17 @@ module Hakyll.Web.Hamlet (
   , renderHtml
   ) where
 
-import Data.Monoid (mempty)
 import Hakyll
 import Data.Text(Text)
-import Text.Hamlet.Runtime
-import qualified Data.Map as Map
+import Text.Hamlet.Runtime (HamletSettings, HamletData, parseHamletTemplate, renderHamletTemplate, HamletSettings, defaultHamletSettings)
+import Data.Map (Map)
 import Text.Blaze.Html.Renderer.String (renderHtml)
 
 hamlCompiler :: Compiler (Item String)
 hamlCompiler = 
   hamlCompilerWith defaultHamletSettings mempty
 
-hamlCompilerWith ::  HamletSettings -> Map.Map Text HamletData -> Compiler (Item String)
+hamlCompilerWith ::  HamletSettings -> Map Text HamletData -> Compiler (Item String)
 hamlCompilerWith hamletSettings hamletVariables =
   getResourceBody >>= renderHaml hamletSettings hamletVariables
 
@@ -27,20 +26,21 @@ hamlTemplateCompiler :: Compiler (Item Template)
 hamlTemplateCompiler =
   hamlTemplateCompilerWith defaultHamletSettings mempty
 
-hamlTemplateCompilerWith :: HamletSettings -> Map.Map Text HamletData -> Compiler (Item Template)
+hamlTemplateCompilerWith :: HamletSettings -> Map Text HamletData -> Compiler (Item Template)
 hamlTemplateCompilerWith hamletSettings hamletVariables =
   cached "Hakyll.Web.Hamlet.hamlTemplateCompiler" $ do
   body <- getResourceBody
   html <- renderHaml hamletSettings hamletVariables body
   return $ readTemplate <$> html
 
-renderHaml :: HamletSettings -> Map.Map Text HamletData -> Item String -> Compiler (Item String)
+renderHaml :: HamletSettings -> Map Text HamletData -> Item String -> Compiler (Item String)
 renderHaml hamletSettings hamletVariables item = do
-  html <- unsafeCompiler . hamlToHtml hamletSettings hamletVariables 
+  html <- unsafeCompiler 
+            . hamlToHtml hamletSettings hamletVariables 
             . itemBody $ item
   makeItem html
 
-hamlToHtml :: HamletSettings -> Map.Map Text HamletData -> String -> IO String
+hamlToHtml :: HamletSettings -> Map Text HamletData -> String -> IO String
 hamlToHtml hamletSettings hamletVariables body = do
     template <- parseHamletTemplate hamletSettings body
     html <- renderHamletTemplate template hamletVariables
